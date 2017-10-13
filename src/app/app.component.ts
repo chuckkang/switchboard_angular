0ff861b9d2c7223930eea8bcf528d6ea5ecb0b7b
Chuck Kang <chuckkang@gmail.com>
2017-10-13 10:55:20 -0700

    Switchboard Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Switchboard';
+  arrBox: Array<buttonStatus> = [];
+  newButton = new buttonStatus(true, "red");
+  setArrBox(){
+		this.arrBox = [
+			new buttonStatus(true, "red"),
+			new buttonStatus(false, "green"),
+			new buttonStatus(true, "red"),
+			new buttonStatus(false, "green"),
+			new buttonStatus(true, "red"),
+		];
+	}
+	
+	flipSwitch(index: number): void{
+		console.log(index, "THIS IS THE INDEX OF THE BUTTON");
+		if (this.arrBox[index].status == false)
+		{
+			this.arrBox[index].status = true;
+			this.arrBox[index].color = "red";
+		} else {
+			this.arrBox[index].status = false;
+			this.arrBox[index].color = "green";
+		}
+	}
+
+	ngOnInit() {
+		this.setArrBox();
+	}
+}
+
+class buttonStatus{
+	status: boolean;
+	color: string;
+	constructor(_status: boolean, _color: string){
+		this.status = _status;
+		this.color =_color;
+	}
 }
